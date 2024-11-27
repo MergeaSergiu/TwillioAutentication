a9b2edd753c758a8977b4d1e97d9b1fa13ecc90e
MergeaSergiu <mergeasergiu@gmail.com>
2024-11-27 19:46:16 +0200

    Added repository to github

---
+++ src/main/java/com/example/twillioautentication/TwillioautenticationApplication.java
@@ -0,0 +1,13 @@
+package com.example.twillioautentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwillioautenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwillioautenticationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/twillioautentication/TwillioautenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.twillioautentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwillioautenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
